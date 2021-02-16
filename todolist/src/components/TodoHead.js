b8bf428a6fbd8c50e9346e7f34ba8118b60c919b
Jisu <kjl0349@naver.com>
2021-02-17 08:54:49 +0900

    <TodoItem> 기능 구현

--- todolist/src/components/TodoHead.js
+++ todolist/src/components/TodoHead.js
@@ -43,7 +43,7 @@ function TodoHead() {
         <TodoHeadBlock>
             <h1>2021년 2월 15일</h1>
             <div className="day">월요일</div>
-            <div className="tasks-left">할 일 2개 남음</div>
+            <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
         </TodoHeadBlock>
     );
 
--- todolist/src/components/TodoItem.js
+++ todolist/src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { MdDone, MdDelete } from 'react-icons/md';
+import { useTodoDispatch } from './TodoContext';
 
 const Remove = styled.div`
     opacity: 0;
@@ -12,6 +13,7 @@ const Remove = styled.div`
     cursor: pointer;
     &:hover {
         color: #ff6b6b;
+        opacity: 1;
     }
 `;
 const CheckCircle = styled.div`
@@ -52,13 +54,24 @@ const TodoItemBlock = styled.div`
 `;
 
 function TodoItem({ id, done, text }) {
+    const dispatch = useTodoDispatch();
+    const onToggle = () =>
+        dispatch({
+            type: 'TOGGLE',
+            id
+        });
+        const onRemove = () =>
+            dispatch({
+                type: 'REMOVE',
+                id
+            });
     return (
         <TodoItemBlock>
-            <CheckCircle done={done}>
+            <CheckCircle done={done} onClick={onToggle}>
                 {done && <MdDone/>}
             </CheckCircle>
             <Text done={done}>{text}</Text>
-            <Remove>
+            <Remove onClick={onRemove}>
                 <MdDelete/>
             </Remove>
         </TodoItemBlock>
